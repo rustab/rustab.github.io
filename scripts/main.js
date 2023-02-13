d55386e24f1ecebe1731ce247c8e630b530ed8fb
rustab <Gustav@Gustavs-MacBook-Air-222.local>
2023-02-13 22:04:07 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -15,14 +15,14 @@ let myHeading = document.querySelector("h1");
 function setUserName() {
     const myName = prompt("Please enter your name.");
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
+    myHeading.textContent = `Hi, ${myName}`;
   }
 
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  myHeading.textContent = `Tross me Daddy-${storedName}!`;
 }
 
 myButton.onclick = () => {
