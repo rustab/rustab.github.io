36b8a7896cdb76b52e4c5e774ef7e7095b61646b
rustab <gustav.lenart@gmail.com>
2023-02-18 13:32:18 +0100

    new navigation pane

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,10 @@
-const myImage = document.querySelector("img");
 
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/dripCoraline.jpg") {
-    myImage.setAttribute("src", "images/Charlie_Jones.jpg");
-  } else {
-    myImage.setAttribute("src", "images/dripCoraline.jpg");
-  }
-};
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function setUserName() {
-    const myName = prompt("Please enter your name.");
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Hi, ${myName}`;
-  }
-
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Trust me Daddy-${storedName}!`;
+/* Handling navigation pane */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
 }
 
-myButton.onclick = () => {
-    setUserName();
-  };
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
